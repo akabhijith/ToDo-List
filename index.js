fa60ac43d27919ccd5209f7216f17aff565a0ce3
akabhijith <75904795+akabhijith@users.noreply.github.com>
2023-08-03 17:51:28 +0530

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+// const ejs=require('ejs');
+
+// app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+let newitems = []
+app.get("/",(req,res)=>{
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today  = new Date();
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("index.ejs",{KindOfDay: day, newListItems:newitems});
+})
+
+app.post("/",(req,res)=>{
+      let newitem=req.body.newitem;
+      newitems.push(newitem);
+     res.redirect("/");
+
+})
+
+app.listen(3000,()=>{
+    console.log("running");
+})
